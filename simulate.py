6aa984bec96180bb5164e23643beeacfbb9b2f4d
James Zampa <james.zampa@gmail.com>
2024-01-10 14:27:04 -0600

    basic viewer

--- simulate.py
+++ simulate.py
@@ -1,1 +1,14 @@
-pass
+import dm_control.mujoco
+import mujoco.viewer
+
+
+def main():
+    m = dm_control.mujoco.MjModel.from_xml_path("example.xml")
+    d = dm_control.mujoco.MjData(m)
+    with mujoco.viewer.launch_passive(m, d) as viewer:
+        viewer.sync()
+        viewer.close()
+
+
+if __name__ == "__main__":
+    main()
